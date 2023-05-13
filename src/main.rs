a88ea5065f369d1c30fc052357d8ded624210d13
devj <power3xz@gmail.com>
2023-05-13 11:05:14 +0900

    qemu exit code 추가

--- src/main.rs
+++ src/main.rs
@@ -38,3 +38,19 @@ fn trivial_assertion() {
     assert_eq!(1, 1);
     println!("[ok]");
 }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
