3bce0e7cd7120fc4510e714722b946ef03060c4e
devj <power3xz@gmail.com>
2023-05-13 11:45:28 +0900

    test case panic 핸들러 추가

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,19 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
