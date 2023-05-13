0ac1807127ee850027a8fbf008c372c8174bdef4
devj <power3xz@gmail.com>
2023-05-13 10:26:47 +0900

    custom_test_frameworks 설정

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 mod vga_buffer;
 
@@ -16,4 +18,12 @@ pub extern "C" fn _start() -> ! {
     println!("Hello world! {}", "!");
 
     loop {}
+}
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
 }
