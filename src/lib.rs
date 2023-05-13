3eed485165c6b3eed4107ca58824ce7dc518d163
devj <power3xz@gmail.com>
2023-05-13 18:19:11 +0900

    interupts.rs 추가

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
+
+pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 
