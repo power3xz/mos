851cb9e0180fa0fa713216f60984e09dba3fd263
devj <power3xz@gmail.com>
2023-05-13 10:34:38 +0900

    test case 추가

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
 
@@ -17,6 +18,9 @@ fn panic(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     println!("Hello world! {}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
@@ -26,4 +30,11 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
