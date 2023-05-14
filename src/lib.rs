266b533f90fdf6fdeced416ec32d94680cca790e
devj <power3xz@gmail.com>
2023-05-14 21:15:39 +0900

    lib.rs hlt_loop 호출

--- src/lib.rs
+++ src/lib.rs
@@ -61,7 +61,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
@@ -69,7 +69,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
