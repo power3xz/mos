8957437ffbab7de1dffe50ba230af67283612bb2
devj <power3xz@gmail.com>
2023-05-14 19:29:07 +0900

    stackoverflow 오류 제거

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,6 @@ pub extern "C" fn _start() -> ! {
     println!("Hello world! {}", "!");
 
     mos::init();
-    fn stack_overflow() {
-        stack_overflow();
-    }
-    stack_overflow();
 
     x86_64::instructions::interrupts::int3();
 
