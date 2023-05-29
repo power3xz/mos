fee2fa7f2e9508d82340f7c59b46619753cf3e76
devj <power3xz@gmail.com>
2023-05-29 18:30:07 +0900

    힙 정보 상수 추가

--- src/allocator.rs
+++ src/allocator.rs
@@ -3,6 +3,9 @@ use core::{
     ptr::null_mut,
 };
 
+pub const HEAP_START: usize = 0x_4444_4444_0000;
+pub const HEAP_SIZE: usize = 100 * 1024;
+
 pub struct Dummy;
 
 unsafe impl GlobalAlloc for Dummy {
